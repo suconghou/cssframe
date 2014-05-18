0467ceaf91f6fb8d5c152c9fbf04b5146e1e80d5
suconghou <1126045770@qq.com>
2014-05-18 15:41:04 +0800

    first

---
+++ src/test.php
@@ -0,0 +1,164 @@
+<!doctype html> 
+<html> 
+<head> 
+    <meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="mycss.css">
+    <style>
+
+    </style> 
+
+
+</head>
+<body>
+    <div id='wrapper' class="container">
+        <div id="a"></div>
+
+        <button class="success">12</button>
+        <button class="danger">按钮</button>
+        <button class="warning">313</button>
+        <button class="info radius">小圆角</button>
+        <button>545</button>
+	<br>
+	    <button class="success sm">12</button>
+        <button class="danger sm">121</button>
+        <button class="warning sm">小号的按钮</button>
+        <button class="info sm">55</button>
+        <button class="sm">545</button>
+<br>
+<input type="button" value="暗暗纳">
+<a href="1" class="btn danger">连接按钮</a>	
+
+        <p class="success">success啊哈哈</p>
+        <p class="danger">danger</p>
+        <p class="info">ingo阿斯顿发生</p>
+        <p class="warning">ingo阿斯顿发生</p>
+
+        <div>
+        	<p class="info">输入框</p>
+        	<input type="text">默认
+        	<input type="text" class="success">success
+        	<input type="text" class="danger">danger
+        	<input type="text" class="info">info
+        	<input type="text" class="warning">warning
+        	<textarea name="11" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div>
+        	<p class="danger">应用于圆角</p>
+        	<input type="text" class="success radius">
+        	<input type="text" class="info round">
+        	<button class="radius">按钮圆角</button>
+        	<button class="round info">按钮圆角大</button>
+        	<button class="radius danger sm">圆角小小</button>
+        </div>
+        <div>
+        	<div class="panel-success">
+        		<div class="panel-header">头部</div>
+        		<div class="panel-body">正文</div>
+        		<div class="panel-footer">尾部</div>
+        	</div>
+        </div>
+        <div>
+        	<span class='success'>180</span>
+        	<span class='danger'>180</span>
+        	<span class='info'>180</span>
+        	<span class='warning'>180</span>
+        	
+        </div>
+        <div>
+        	<div class="alert">
+        		这是一个警告框
+        	</div>
+        	<div class="alert success">
+        		这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框
+        	</div>
+        	<div class="alert danger">
+        		这是一个警告框
+        	</div>
+        	<div class="alert info">
+        		这是一个警告框
+        	</div>
+        	<div class="alert warning">
+        		这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框这是一个警告框
+        	</div>
+        	<div class="alert success radius">
+        		这是一个警告框,小圆角
+        	</div>
+        	<div class="alert info round">
+        		这是一个警告框,大圆角
+        	</div>
+        </div>
+        <div>
+        	<p class="warning">列表</p>
+        	
+        </div>
+        <div>
+        	<p class="info">面板</p>
+        	<div class="panel success radius">
+        		<div class="header">标题</div>
+        		<div class="body">正文</div>
+        	</div>
+        	<div class="panel danger radius">
+        		<div class="header">标题</div>
+        		<div class="body">
+        			正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文正文
+        		</div>
+        	</div>
+        	<div class="panel info radius">
+        		<div class="header">标题</div>
+        		<div class="body">正文</div>
+        	</div>
+        	<div class="panel warning ">
+        		<div class="header">标题</div>
+        		<div class="body">正文</div>
+        	</div>
+        </div>
+        <div>
+        	<p class="info">分页</p>
+        	<ul class="pagination circle">
+        		<li><a href="">1</a></li>
+        		<li><a href="">2</a></li>
+        		<li class="active"><a href="">3</a></li>
+        		<li><a href="">4</a></li>
+        		<li><a href="">5</a></li>
+        		<li><a href="">16</a></li>
+        	</ul>
+        	<ul class="pagination">
+        		<li><a href="">1</a></li>
+        		<li><a href="">2</a></li>
+        		<li class="active"><a href="">3</a></li>
+        		<li><a href="">4</a></li>
+        		<li><a href="">5</a></li>
+        		<li><a href="">16</a></li>
+        	</ul>
+        </div>
+    </div><!-- end container -->
+
+    <div class="container">
+
+        
+    </div>
+
+<div class="container">
+  <div class="row clearfix">
+    <div class="column half">
+      <!-- Your Content -->
+    </div>
+    <div class="column half">
+      <!-- Your Content -->
+    </div>
+  </div>
+  
+  <div class="row clearfix">
+    <div class="column half">
+      <!-- Your Content -->
+    </div>
+    <div class="column half">
+      <!-- Your Content -->
+    </div>
+  </div>
+</div>
+<script src="http://cdn.bootcss.com/jquery/2.0.3/jquery.min.js"></script>
+</body>
+</html>
